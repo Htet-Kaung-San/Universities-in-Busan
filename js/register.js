315d05fc11770b991e0d8b1125be3fb52460d1f2
Htet-Kaung-San <htetsan206@gmail.com>
2025-06-15 19:38:07 +0900

    project updated

--- js/register.js
+++ js/register.js
@@ -3,8 +3,8 @@ document.getElementById('email').addEventListener('blur', function() {
   const emailCheckDiv = document.getElementById('email-check');
   emailCheckDiv.textContent = '';
   if (!email) return;
-  fetch(`/check-email?email=${encodeURIComponent(email)}`) // js var = user+test@example.com -> user%2Btest%40example.com
-    .then(res => res.json()) // then is promise chain, takes HTTP response from fetch and parses as JSON.
+  fetch(`/check-email?email=${encodeURIComponent(email)}`)
+    .then(res => res.json())
     .then(data => {
       if (data.exists) {
         emailCheckDiv.textContent = 'Email is already registered.';
@@ -21,16 +21,15 @@ document.getElementById('registerForm').addEventListener('submit', function(e) {
 
   if (pw.length < 8) {
     errorDiv.textContent = 'Password must be at least 8 characters long.';
-    e.preventDefault(); // keep the user on the same page and show error messages instead of submitting invalid data
+    e.preventDefault(); 
     return;
   }
   if (pw !== cpw) {
-    e.preventDefault(); // if error happens for sure, order don't matter since individual actions
+    e.preventDefault();
     errorDiv.textContent = 'Passwords do not match.';
     return;
   }
   if (emailCheckDiv.textContent) {
-    // errorDiv.textContent = 'Please use a different email address.';
     e.preventDefault();
     return;
   }
